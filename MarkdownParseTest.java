d6e4446e7b5171ed9c6ea3c361fcac3dd230797d
anselvar <103392537+anselvar@users.noreply.github.com>
2022-05-30 20:40:25 -0700

    test files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,215 +20,181 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void Snippet1() throws IOException {
+        String contents = Files.readString(Path.of("snippet1.md"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
-    /*
-    / Test markdown file w/ two links present
-    */
     @Test
-    public void testMarkdownParseFile1() {
-        Path filePath = Path.of("test-file.md");
-
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
-
-            for (int i = 0; i < parsedLinks.size(); ++i) {
-                assertEquals(expectedlinks.get(i), parsedLinks.get(i));
-            }
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 1");
-        }
-
+    public void Snippet2() throws IOException {
+        String contents = Files.readString(Path.of("snippet2.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
-
-    /*
-    / Test markdown file w/ two links and an image present
-    */
+    
     @Test
-    public void testMarkdownParseFile2() {
-        Path filePath = Path.of("test-file2.md");
-
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
-
-            for (int i = 0; i < parsedLinks.size(); ++i) {
-                assertEquals(expectedlinks.get(i), parsedLinks.get(i));
-            }
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 2");
-        }
-
+    public void Snippet3() throws IOException {
+        String contents = Files.readString(Path.of("snippet3.md"));
+        List<String> expect = List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", "https://cse.ucsd.edu/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
+    // @Test
+    // public void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
 
-    /*
-    / Test markdown file w/ two links, an image present, and no title
-    */
-    @Test
-    public void testMarkdownParseFile3() {
-        Path filePath = Path.of("test-file3.md");
+    // /*
+    // / Test markdown file w/ two links present
+    // */
+    // @Test
+    // public void testMarkdownParseFile1() {
+    //     Path filePath = Path.of("test-file.md");
 
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-            for (int i = 0; i < parsedLinks.size(); ++i) {
-                assertEquals(expectedlinks.get(i), parsedLinks.get(i));
-            }
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 3");
-        }
+    //         for (int i = 0; i < parsedLinks.size(); ++i) {
+    //             assertEquals(expectedlinks.get(i), parsedLinks.get(i));
+    //         }
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 1");
+    //     }
 
-    }
+    // }
 
-    /*
-    / Test markdown file w/ two links and an image present, with extra space at the end
-    */
-    @Test
-    public void testMarkdownParseFile4() {
-        Path filePath = Path.of("test-file4.md");
+    // /*
+    // / Test markdown file w/ two links and an image present
+    // */
+    // @Test
+    // public void testMarkdownParseFile2() {
+    //     Path filePath = Path.of("test-file2.md");
 
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-            for (int i = 0; i < parsedLinks.size(); ++i) {
-                assertEquals(expectedlinks.get(i), parsedLinks.get(i));
-            }
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 4");
-        }
+    //         for (int i = 0; i < parsedLinks.size(); ++i) {
+    //             assertEquals(expectedlinks.get(i), parsedLinks.get(i));
+    //         }
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 2");
+    //     }
 
-    }
+    // }
 
-    /*
-    / Test markdown file w/ no links present
-    */
-    @Test
-    public void testMarkdownParseFile5() {
-        Path filePath = Path.of("test-file5.md");
+    // /*
+    // / Test markdown file w/ two links, an image present, and no title
+    // */
+    // @Test
+    // public void testMarkdownParseFile3() {
+    //     Path filePath = Path.of("test-file3.md");
 
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-            assertEquals(0, parsedLinks.size());
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 5");
-        }
+    //         for (int i = 0; i < parsedLinks.size(); ++i) {
+    //             assertEquals(expectedlinks.get(i), parsedLinks.get(i));
+    //         }
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 3");
+    //     }
 
-    }
+    // }
 
-    /*
-    / Test markdown file w/ no links present, but empty brackets are present
-    */
-    @Test
-    public void testMarkdownParseFile6() {
-        Path filePath = Path.of("test-file6.md");
+    // /*
+    // / Test markdown file w/ two links and an image present, with extra space at the end
+    // */
+    // @Test
+    // public void testMarkdownParseFile4() {
+    //     Path filePath = Path.of("test-file4.md");
 
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-            assertEquals(0, parsedLinks.size());
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 6");
-        }
+    //         for (int i = 0; i < parsedLinks.size(); ++i) {
+    //             assertEquals(expectedlinks.get(i), parsedLinks.get(i));
+    //         }
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 4");
+    //     }
 
-    }
+    // }
 
-    /*
-    / Test markdown file w/ no links present, but lines that look like links
-    */
-    @Test
-    public void testMarkdownParseFile7() {
-        Path filePath = Path.of("test-file7.md");
+    // /*
+    // / Test markdown file w/ no links present
+    // */
+    // @Test
+    // public void testMarkdownParseFile5() {
+    //     Path filePath = Path.of("test-file5.md");
 
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-            assertEquals(0, parsedLinks.size());
-        } catch (Exception e) {
-            System.out.println("Error: file not found 7!");
-        }
+    //         assertEquals(0, parsedLinks.size());
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 5");
+    //     }
 
-    }
+    // }
 
-    /*
-    / Test markdown file w/ only ")[" present, not a link
-    */
-    @Test
-    public void testMarkdownParseFile8() {
-        Path filePath = Path.of("test-file8.md");
+    // /*
+    // / Test markdown file w/ no links present, but empty brackets are present
+    // */
+    // @Test
+    // public void testMarkdownParseFile6() {
+    //     Path filePath = Path.of("test-file6.md");
 
-        try {
-            String fileContents = Files.readString(filePath);
-            ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-            assertEquals(0, parsedLinks.size());
-        } catch (Exception e) {
-            System.out.println("Error: file not found! 8");
-        }
+    //         assertEquals(0, parsedLinks.size());
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 6");
+    //     }
 
-    }
+    // }
 
-    /*
-    / Test markdown snippet #1
-    */
-    @Test
-    public void testMarkdownParseSnippet1() throws IOException {
+    // /*
+    // / Test markdown file w/ no links present, but lines that look like links
+    // */
+    // @Test
+    // public void testMarkdownParseFile7() {
+    //     Path filePath = Path.of("test-file7.md");
 
-        String fileContents = Files.readString(Path.of("test-file-snippet1.md"));
-        ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
-        ArrayList<String> expectedSnip1Links = new ArrayList<>();
-        expectedSnip1Links.add("`google.com");
-        expectedSnip1Links.add("google.com");
-        expectedSnip1Links.add("ucsd.edu");
-        for (int i = 0; i < 3; ++i) {
-            assertEquals(expectedSnip1Links.get(i), parsedLinks.get(i));
-        }
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-    }
+    //         assertEquals(0, parsedLinks.size());
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found 7!");
+    //     }
 
-    /*
-    / Test markdown snippet #2
-    */
-    @Test
-    public void testMarkdownParseSnippet2() throws IOException {
+    // }
 
-        String fileContents = Files.readString(Path.of("test-file-snippet2.md"));
-        ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
-        ArrayList<String> expectedSnip2Links = new ArrayList<>();
-        expectedSnip2Links.add("a.com");
-        expectedSnip2Links.add("a.com(())");
-        expectedSnip2Links.add("example.com");
-        for (int i = 0; i < 3; ++i) {
-            assertEquals(expectedSnip2Links.get(i), parsedLinks.get(i));
-        }
+    // /*
+    // / Test markdown file w/ only ")[" present, not a link
+    // */
+    // @Test
+    // public void testMarkdownParseFile8() {
+    //     Path filePath = Path.of("test-file8.md");
 
-    }
+    //     try {
+    //         String fileContents = Files.readString(filePath);
+    //         ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
 
-    /*
-    / Test markdown snippet #3
-    */
-    @Test
-    public void testMarkdownParseSnippet3() throws IOException {
+    //         assertEquals(0, parsedLinks.size());
+    //     } catch (Exception e) {
+    //         System.out.println("Error: file not found! 8");
+    //     }
 
-        String fileContents = Files.readString(Path.of("test-file-snippet3.md"));
-        ArrayList<String> parsedLinks = MarkdownParse.getLinks(fileContents);
-        ArrayList<String> expectedSnip3Links = new ArrayList<>();
-        expectedSnip3Links.add("https://www.twitter.com");
-        expectedSnip3Links.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
-        expectedSnip3Links.add("https://cse.ucsd.edu/");
-        for (int i = 0; i < 3; ++i) {
-            assertEquals(expectedSnip3Links.get(i), parsedLinks.get(i));
-        }
-
-    }
+    // }
 
 
 }
