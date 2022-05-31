d039de0ee1c92c8c2c0ca663d361ad023915ce77
anselvar <103392537+anselvar@users.noreply.github.com>
2022-05-30 20:18:14 -0700

    libs

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,40 +8,23 @@ import java.util.List;
 import org.junit.*;
 public class MarkdownParseTest {
 
+ 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
-    public void newFile2Test() throws IOException{
-        String markdown = Files.readString(Path.of("new-file-2.md"));
-        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
-    }
-
-    @Test
-    public void newFile5Test() throws IOException{
-        String markdown = Files.readString(Path.of("new-file-5.md"));
-        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
-    }
-
-    //For lab report week 4
-    @Test
-    public void LabReportTest1() throws IOException {
+    public void Snippet1() throws IOException {
         String contents = Files.readString(Path.of("lab-report-4-1.md"));
         List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
-    public void LabReportTest2() throws IOException {
+    public void Snippet2() throws IOException {
         String contents = Files.readString(Path.of("lab-report-4-2.md"));
         List<String> expect = List.of("a.com", "a.com(())", "example.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
     
     @Test
-    public void LabReportTest3() throws IOException {
+    public void Snippet3() throws IOException {
         String contents = Files.readString(Path.of("lab-report-4-3.md"));
         List<String> expect = List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", "https://cse.ucsd.edu/");
         assertEquals(expect, MarkdownParse.getLinks(contents));
